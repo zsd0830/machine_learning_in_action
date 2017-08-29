60e4cf266d579db8d6a67f2037016ba9131a42f4
zsd0830 <zsd0830@gmail.com>
2017-08-29 21:26:16 +0800

    chapter04 changed

--- Ch04/test.py
+++ Ch04/test.py
@@ -1,7 +1,10 @@
 from numpy import *
-import bayes
+from Ch04 import bayes
 
 listOPosts, listClasses = bayes.loadDataSet()
+#得到词条listOPosts,每个词条的label listClasses
+
+
 
 myVocabList = bayes.createVocabList(listOPosts)
 
@@ -11,3 +14,5 @@ for postinDoc in listOPosts:
     trainMat.append(bayes.setOfWords2Vec(myVocabList, postinDoc))
 
 p0V, p1V, pAb = bayes.trainNB0(trainMat, listClasses)
+#p0V表示各种词汇出现在非侮辱性文档中的概率;p1V各种词汇出现在侮辱性文档中的概率
+
